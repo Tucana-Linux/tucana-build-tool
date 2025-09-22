00f34efa2c652699ff1efb866dda2361f4465e5a
Rahul Chandra <rahul@chandra.net>
2025-09-21 22:55:31 -0700

    Add config file

--- install.sh
+++ install.sh
@@ -6,6 +6,9 @@ fi
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 mkdir -p $DESTDIR/usr/bin
+mkdir -p $DESTDIR/etc/tucana-build-tool
+cp example.conf $DESTDIR/etc/tucana-build-tool/build.conf
 cp tucana-build-tool.sh $DESTDIR/usr/bin/tbt
+
 
 
